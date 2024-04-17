f03e34c402ad6a6de8b66541d6eec5912a1393b7
Georgeches <georgechesire83@gmail.com>
2024-04-17 10:25:58 +0300

    update image src

--- resources/views/components/productSmall.blade.php
+++ resources/views/components/productSmall.blade.php
@@ -3,7 +3,7 @@
 <div class="style-card-small p-0 border-0">
     <div class="card-body p-0">
         <div class="card-body-image p-0">
-            <a href="/products/{{$product['id']}}"><img class="w-100" src='{{ asset("storage/$product->image") }}' alt=""/></a>
+            <a href="/products/{{$product['id']}}"><img class="w-100" src='storage/{{$product->image}}' alt=""/></a>
         </div>
         <div class="product-details px-1 container text-start">
             <div>
